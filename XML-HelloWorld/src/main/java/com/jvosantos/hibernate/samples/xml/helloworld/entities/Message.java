bedf70bba970dac84aae9f3e88e8aaf4a5b0d746
Vasco Santos <jvosantos@gmail.com>
2015-03-25 23:08:32 +0000

    Added XML based mapping hello world sample

--- XML-HelloWorld/src/main/java/com/jvosantos/hibernate/samples/xml/helloworld/entities/Message.java
+++ XML-HelloWorld/src/main/java/com/jvosantos/hibernate/samples/xml/helloworld/entities/Message.java
@@ -3,28 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.jvosantos.hibernate.samples.annotation.helloworld.entities;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+package com.jvosantos.hibernate.samples.xml.helloworld.entities;
 
 /**
  *
  * @author Vasco Santos
  */
-@Entity
-@Table(name = "MESSAGE")
 public class Message {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "ID")
     private Long id;
-    @Column(name = "TEXT")
     private String text;
 
     public Message() {
